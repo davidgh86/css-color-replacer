3f4bed8aed7707bd78e09dc32501d87fc45a359f
david <david_gh86@hotmail.es>
2022-08-21 13:54:44 +0200

    allow iframe

--- src/components/HtmlColorReplacer.vue
+++ src/components/HtmlColorReplacer.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <div><input type="text" v-model="aaaa" /></div>
+    <div><input type="text" v-model="htmlContent" /></div>
     <div>
       <button type="button" @click="updateUrlContent($event)">Click Me!</button>
     </div>
-    <div v-html="urlContent"></div>
+    <div>
+      <iframe :src="srcIFrame" sandbox="allow-same-origin"></iframe>
+    </div>
     <ColorReplacer />
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
-import { getUrlContent } from "../services/client";
+import { ref, computed } from "vue";
 import ColorReplacer from "./ColorReplacer.vue";
+import { fromByteArray } from "base64-js/index";
 
 export default {
   name: "HtmlColorReplacer",
@@ -20,18 +22,57 @@ export default {
     ColorReplacer,
   },
   setup() {
-    const aaaa = ref("");
-    const urlContent = ref("");
+    const htmlContent = ref("");
+    const urlContent = ref(
+      `<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: lightblue;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+}
+</style>
+</head>
+<body>
+
+<h1>My First CSS Example</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>`
+    );
+    const srcIFrame = computed(() => {
+      const utf8Encode = new TextEncoder();
+      const byteArray = utf8Encode.encode(urlContent.value);
+      const dat = fromByteArray(byteArray);
+      return `data:text/html;base64,${dat}`;
+    });
     const updateUrlContent = () => {
-      alert(aaaa.value);
-      getUrlContent(aaaa.value).then((content) => {
-        urlContent.value = content;
-      });
+      // getUrlContent(htmlContent.value).then((content) => {
+      //   urlContent.value = content;
+      // });
+      urlContent.value =
+        "<div><style>.afs{background-color: red;}</style><div class='afs'>hola</div></div>";
+      // document
+      //   .querySelector("native-hello-world.uno")
+      //   .setAttribute("css", urlContent.value);
+      //urlContent.value = `adfsfdasf`;
     };
     return {
-      aaaa,
+      htmlContent,
       urlContent,
       updateUrlContent,
+      srcIFrame,
     };
   },
 };
